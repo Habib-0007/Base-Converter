22f7c9868451ba5e16a78537c3d4cc4232cfce96
Habib-0007 <adebayohabib7@gmail.com>
2023-12-07 09:11:18 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,57 @@
+let baseNum = document.querySelector(".base-num");
+let inputBase = document.querySelector("#base-input");
+let outputBase = document.querySelector("#base-output");
+let convertBtn = document.querySelector(".conv-container button");
+let convRes = document.querySelector(".result");
+
+let convertNumber = () => {
+	let numVal = baseNum.value;
+	let inputBaseVal = inputBase.value;
+	let outputBaseVal = outputBase.value;
+	let result = "";
+	if (inputBaseVal == "BIN") {
+		if (outputBaseVal == "BIN") {
+			result = parseInt(numVal, 2).toString(2);
+		} else if (outputBaseVal == "OCT") {
+			result = parseInt(numVal, 2).toString(8);
+		} else if (outputBaseVal == "DEC") {
+			result = parseInt(numVal, 2).toString(10);
+		} else if (outputBaseVal == "HEX") {
+			result = parseInt(numVal, 2).toString(16);
+		}
+	} else if (inputBaseVal == "OCT") {
+		if (outputBaseVal == "BIN") {
+			result = parseInt(numVal, 8).toString(2);
+		} else if (outputBaseVal == "OCT") {
+			result = parseInt(numVal, 8).toString(8);
+		} else if (outputBaseVal == "DEC") {
+			result = parseInt(numVal, 8).toString(10);
+		} else if (outputBaseVal == "HEX") {
+			result = parseInt(numVal, 8).toString(16);
+		}
+	} else if (inputBaseVal == "DEC") {
+		if (outputBaseVal == "BIN") {
+			result = parseInt(numVal, 10).toString(2);
+		} else if (outputBaseVal == "OCT") {
+			result = parseInt(numVal, 10).toString(8);
+		} else if (outputBaseVal == "DEC") {
+			result = parseInt(numVal, 10).toString(10);
+		} else if (outputBaseVal == "HEX") {
+			result = parseInt(numVal, 10).toString(16);
+		}
+	} else if (inputBaseVal == "HEX") {
+		if (outputBaseVal == "BIN") {
+			result = parseInt(numVal, 16).toString(2);
+		} else if (outputBaseVal == "OCT") {
+			result = parseInt(numVal, 16).toString(8);
+		} else if (outputBaseVal == "DEC") {
+			result = parseInt(numVal, 16).toString(10);
+		} else if (outputBaseVal == "HEX") {
+			result = parseInt(numVal, 16).toString(16);
+		}
+	}
+	convRes.textContent = `${inputBaseVal}: ${numVal} equals ${outputBaseVal}:
+	${result}`;
+};
+
+convertBtn.addEventListener("click", convertNumber);
